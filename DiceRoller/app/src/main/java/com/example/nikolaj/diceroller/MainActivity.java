6614f42135b5ab978078fb70e23deec76e522a0a
nickzr <niko2460@easv365.dk>
2016-03-03 16:49:48 +0100

    Initial commit

---
+++ DiceRoller/app/src/main/java/com/example/nikolaj/diceroller/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nikolaj.diceroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
